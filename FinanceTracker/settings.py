71c9c259b7307d98ca806543917dc48272eb21f8
BatsCrow <156543086+BatsCrow@users.noreply.github.com>
2025-05-19 14:41:23 -0400

    Added Login Page

--- FinanceTracker/settings.py
+++ FinanceTracker/settings.py
@@ -126,6 +126,8 @@ STATIC_URL = 'static/'
 CRISPY_ALLOWED_TEMPLATE_PACK = 'bootstrap5'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
+LOGIN_REDIRECT_URL = "/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
