284f5b359ef2eee262a09dec0f5c7fed8a743f81
BatsCrow <156543086+BatsCrow@users.noreply.github.com>
2025-05-19 11:54:28 -0400

    Added Vertical SideBar

--- main/views.py
+++ main/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(response):
-    return HttpResponse("<h1> Hello, World! </h1>")
+    return render(response, "main/index.html", {})
