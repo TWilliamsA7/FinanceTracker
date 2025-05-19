aa4870750c384436e88d5b60e0d82ccb4eb4cc1f
BatsCrow <156543086+BatsCrow@users.noreply.github.com>
2025-05-19 12:53:57 -0400

    Added Register Form

--- FinanceTracker/urls.py
+++ FinanceTracker/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from register import views as rv
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("main.urls")),
+    path("register/", rv.register, name="register"),
 ]
